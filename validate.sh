929579df928a18ec5572571409c9b1c3c9a9eb54
Gheorghe <gheorghe.popescu@aserto.com>
2021-12-13 11:27:26 +0200

    update policy release workflow for beta

--- validate.sh
+++ validate.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 echo "reflect over authorizer service"
-grpcurl aona.eng.aserto.com:8443 list | xargs -n 1 -I {} grpcurl aona.eng.aserto.com:8443 list {} > perms.txt
+grpcurl aona.beta.aserto.com:8443 list | xargs -n 1 -I {} grpcurl aona.beta.aserto.com:8443 list {} > perms.txt
 echo "reflect over tenant service"
-grpcurl tenant.eng.aserto.com:8443 list | xargs -n 1 -I {} grpcurl tenant.eng.aserto.com:8443 list {} >> perms.txt
+grpcurl tenant.beta.aserto.com:8443 list | xargs -n 1 -I {} grpcurl tenant.beta.aserto.com:8443 list {} >> perms.txt
 echo "reflect over decision logs service"
-grpcurl decision-logs.eng.aserto.com:8443 list | xargs -n 1 -I {} grpcurl decision-logs.eng.aserto.com:8443 list {} >> perms.txt
+grpcurl decision-logs.beta.aserto.com:8443 list | xargs -n 1 -I {} grpcurl decision-logs.beta.aserto.com:8443 list {} >> perms.txt
 echo "sort"
 sort -o perms.txt{,}
 
@@ -20,18 +20,18 @@ while read perm; do
 
 	# check if permission is in the perms table
 	x=$(jq --arg P "$perm" '.perms | has($P) ' ./src/perms/data.json)
-	if [ ${x} == "false" ]; then 
+	if [ ${x} == "false" ]; then
 		echo perm $perm missing from perms map
 	fi
 
 	f=false
 	for role in $(jq -r '.roles | keys | .[]' src/roles/data.json); do
-		x=$(jq --arg R "$role" --arg P "$perm" '.roles | .[$R] | .perms | has($P) ' ./src/roles/data.json)		
+		x=$(jq --arg R "$role" --arg P "$perm" '.roles | .[$R] | .perms | has($P) ' ./src/roles/data.json)
 		if [ $x == "true" ]; then
 			f=true
 		fi
 	done
-    
+
 	if [ $f == "false" ]; then
 		echo perm $perm appears in no roles
 	fi
